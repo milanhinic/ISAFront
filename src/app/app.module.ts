438b000cab2aa6812f25a8ed7b08d36f850c7860
trpelonski <trpovski@gmail.com>
2018-03-23 19:50:50 +0100

    Izgled register i login stranica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 
 import { AppComponent } from './app.component';
 import { CentralComponent } from './central/central.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CentralComponent,
+    RegisterComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -19,9 +24,19 @@ import { CentralComponent } from './central/central.component';
         {
           path : '',
           component : CentralComponent
+        },
+        {
+          path : 'register',
+          component : RegisterComponent
+        },
+        {
+          path : 'login',
+          component : LoginComponent
         }
       ]
-     )
+     ),
+     FormsModule,
+     ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
