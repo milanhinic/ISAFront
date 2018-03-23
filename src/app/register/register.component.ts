e10ed5a63834d4d5ea634ee46e13da8742863944
trpelonski <trpovski@gmail.com>
2018-03-23 22:15:10 +0100

    Velik deo validacije za registraciju odradjen

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  registracijaForma;
+
   constructor() { }
 
   ngOnInit() {
+    this.registracijaForma = new FormGroup({
+      email : new FormControl("",Validators.compose([
+        Validators.required,
+        Validators.pattern('[a-zA-z0-9._]{0,64}@[a-z]{2,10}(\.[a-z]{2,10})+')
+      ])),
+      sifra : new FormControl("",Validators.compose([
+        Validators.required,
+        Validators.minLength(8)
+      ])),
+      sifraPotvrda : new FormControl("",Validators.compose([
+        Validators.required,
+        Validators.minLength(8)
+      ])),
+      ime : new FormControl("",Validators.required),
+      prezime : new FormControl("",Validators.required),
+      grad : new FormControl("",Validators.required),
+      telefon : new FormControl("",Validators.compose([
+        Validators.required,
+        Validators.pattern('\\+?[0-9]{0,12}')
+      ]))
+    })
+  }
+
+  registruj = function(korisnik){
+    console.log(korisnik);
   }
 
 }
